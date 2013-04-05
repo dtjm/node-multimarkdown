80c14ea8e8bf600cea041349933d5492566969a1
Sam Nguyen <samxnguyen@gmail.com>
2013-04-04 22:40:16 -0700

    updating test

--- test.js
+++ test.js
@@ -5,17 +5,24 @@ var expected = {
     "ab": "<p>ab</p>",
     "c": "<p>c</p>",
     "å": "<p>å</p>",
-    "abc\n--": "<h2>abc</h2>"
+    "abc\n--": "<h2 id=\"abc\">abc</h2>"
 };
 
 console.log("MultiMarkdown Test");
 
+var failed = false;
+
 for(var input in expected){
     var out = MMD.convert(input);
     if(expected[input] !== out) {
+        failed = true;
         console.log("FAIL");
         console.log("input:", input, "expected:", expected[input], "actual:", out);
     } else {
         console.log("OK  ");
     }
+}
+
+if (failed) {
+    process.exit(1);
 }
