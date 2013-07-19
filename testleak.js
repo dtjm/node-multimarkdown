c2cd554eae24bcf0bb233746d3acdaf0575e60b3
Sam Nguyen <samxnguyen@gmail.com>
2013-07-18 23:12:26 -0700

    Change test to non-infinite loop

--- testleak.js
+++ testleak.js
@@ -2,7 +2,7 @@ var MMD = require("./build/Release/multimarkdown.node");
 
 var i = 0;
 var input = "";
-while(true){
+while(i < 1000000){
     //console.log(i+=10);
     MMD.convert(input);
     input += "aaaaaaaaaa";
